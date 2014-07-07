1fabbd57947ce07029fb72886286365f6da6c499
ansh0l <anshl.goyl@gmail.com>
2014-07-07 15:38:52 +0530

    prob 14 solved

---
+++ 014/longest_collatz_sequence.py
@@ -0,0 +1,29 @@
+chain_length_dict = {1 : 1}
+MILLION = 1000000
+
+def next_term(n):
+    if n%2 == 0:
+        return n/2
+    else:
+        return 3*n + 1
+
+def find_chain_length(n):
+    global chain_length_dict
+    if chain_length_dict.has_key(n):
+        return chain_length_dict[n]
+    else:
+        chain_length = 1 + find_chain_length(next_term(n))
+        chain_length_dict[n] = chain_length
+        return chain_length
+
+
+def find_longest_chain():
+    global chain_length_dict
+    longest_chain_source, longest_chain_length = None, 0
+    for num in range(2, MILLION): 
+        chain_length = find_chain_length(num)
+        if chain_length > longest_chain_length:
+            longest_chain_source, longest_chain_length = num, chain_length
+    return longest_chain_source, longest_chain_length
+
+print find_longest_chain()
