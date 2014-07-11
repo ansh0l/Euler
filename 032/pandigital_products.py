9a7485418f281fdf0f9b24335e603a0cad858cf7
ansh0l <anshl.goyl@gmail.com>
2014-07-11 19:46:52 +0530

    prob 32 comment and redundant function removal

--- 032/pandigital_products.py
+++ 032/pandigital_products.py
@@ -1,10 +1,6 @@
-def is_pandigital(a, b): #1 through 9 pandigital; a*b=c
-    string = str(a)+str(b)+str(a*b)
-    return len(string) == 9 and set(string) == set("123456789")
-
 def main():
     pandigital = []
-    for a in range(10000):
+    for a in range(10000): #10000, since 9876*5=49380 => total digits exceed 9
         for b in range(10000):
             string = str(a)+str(b)+str(a*b)
             if len(string) > 9:
@@ -12,7 +8,6 @@ def main():
             elif set(string) == set("123456789") and a*b not in pandigital:
                 pandigital.append(a*b)
     print sum(pandigital)
-
 
 if __name__ == "__main__":
     main()
