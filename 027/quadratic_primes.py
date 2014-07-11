2d24a805a371cc8b5bcad0285f92f2b46d5fa9d1
ansh0l <anshl.goyl@gmail.com>
2014-07-11 15:13:11 +0530

    prob 27 solved

--- 027/quadratic_primes.py
+++ 027/quadratic_primes.py
@@ -1,9 +1,9 @@
-limit = 1000
+LIMIT = 1000
 primes = [2]
 
 def get_primes_less_than_1000():
     candidate = 3
-    while candidate < limit:
+    while candidate < LIMIT:
         can_be_prime = True
         for prime in primes:
             if candidate % prime == 0:
@@ -13,15 +13,12 @@ def get_primes_less_than_1000():
             primes.append(candidate)
         candidate += 2
 
-def main():
-    get_primes_less_than_1000()
-
 def is_quadratic_prime(a, b, n):
     return True if n**2 + a*n + b in primes else False
 
 def get_range_a():
     range_a = []
-    for i in range(limit):
+    for i in range(LIMIT):
         range_a += [i, i * -1] 
     return range_a
 
@@ -31,17 +28,21 @@ def get_range_b():
         range_b += [p, p * -1] 
     return range_b
 
-def get_main():
+def main():
+    get_primes_less_than_1000()
     range_a, range_b = get_range_a(), get_range_b()
-    longest_streak, largest_ab = 0, 0
+    ab = {'a': None, 'b': None, 'ab': 0, 'n': 0}
     for a in range_a:
         for b in range_b:
-            for n in range(limit):
+            for n in range(LIMIT):
                 if not is_quadratic_prime(a, b, n):
-                    if n >= longest_streak and a*b > largest_ab:
-                        longest_streak = n
-                        largest_ab = a*b
-    print largest_ab
+                    if n >= ab['n']:
+                        ab['n'] = n
+                        ab['a'] = a
+                        ab['b'] = b
+                        ab['ab'] = a*b
+                    break
+    print ab
 
 if __name__ == "__main__":
     main()
