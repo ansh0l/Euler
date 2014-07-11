0440061018e53422ccefe6d93acd4a917b0733c7
ansh0l <anshl.goyl@gmail.com>
2014-07-11 13:54:42 +0530

    prob 26 solved

--- 026/reciprocal_cycles.py
+++ 026/reciprocal_cycles.py
@@ -1,15 +1,22 @@
-
 #dividend / divisor = quotient * divisor + remainder
+#Anytime a remainder repeats, means quotient is going to repeat
 
-highest_repetition_qr_dict = {}
+highest_repetition_qr_dict = {'q':'0.', 'r':[], 'n': 0}
 for divisor in range(2, 1000):
-    qr_dict = {'q':'', 'r':''}
-    dividend = 10 if d < 10 else (100 if d < 100 else 1000)
+    qr_dict = {'q':'', 'r':[], 'n': divisor}
+    dividend = 10 if divisor < 10 else (100 if divisor < 100 else 1000)
+    remainder, quotient = None, None
+    repetitive = False
     while dividend % divisor !=0:
         remainder = dividend % divisor
         quotient = dividend / divisor
         qr_dict['q'] += str(quotient)
-        qr_dict['r'] += str(remainder)
+        qr_dict['r'].append(remainder)
         dividend = remainder * 10
-        if remainder == initial_remainder && quotient == initial_quotient:
-            break #cycle is here
+        if qr_dict['r'].count(remainder) > 1: #cycle is here
+            repetitive = True
+            break
+    if repetitive and len(qr_dict['q']) > len(highest_repetition_qr_dict['q']):
+        highest_repetition_qr_dict = qr_dict
+
+print highest_repetition_qr_dict
