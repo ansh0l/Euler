c01a817e6e805ce3de3435dfd363223f73c4ecba
ansh0l <anshl.goyl@gmail.com>
2014-07-11 12:28:10 +0530

    prob 23 solved

--- 023/non_abundant_sums.py
+++ 023/non_abundant_sums.py
@@ -1,56 +1,28 @@
-#limit = 280 + 1
-limit = 28123 + 1
-
-prime_numbers = [2]
-for num in range(3, limit):
-    is_prime = True
-    for prime in prime_numbers:
-        if num % prime == 0:
-            is_prime = False
-            break
-    if is_prime:
-        prime_numbers.append(num)
+#from find_abundant_numbers import * 
+from constant import *
 
 def get_abundant_numbers():
-    abundant_numbers = []
-    for num in range(2, limit):
-        divisors = []
-        if num in prime_numbers:
-            continue
-        for candidate in range(1, num/2 + 1):
-            if num % candidate == 0:
-                divisors.append(candidate)
-        if sum(divisors) > num:
-            abundant_numbers.append(num)
-            print "%5d %6d %3d" %(num, sum(divisors), len(divisors))
+    with open("abundant_numbers.txt", "r") as f:
+        abundant_numbers = [int(val) for val in f.read().split()]
     return abundant_numbers
 
-def is_a_sum_of_abundant_numbers(num, abundant_numbers, idx_end): #a+b = k problem
-    idx_st = 0
-    while idx_st < idx_end:
-        if abundant_numbers[idx_st] + abundant_numbers[idx_end] == num:
-            return True
-        elif abundant_numbers[idx_st] + abundant_numbers[idx_end] < num:
-            idx_st += 1
-        else:
-            idx_end -= 1
-    return False
-    
+def get_abundant_sum_numbers(abundant_numbers):
+    length = len(abundant_numbers)
+    abundant_sum_numbers = {}
+    for idx, num1 in enumerate(abundant_numbers):
+        for num2 in abundant_numbers[idx:length]:
+            if num1 + num2 >= limit:
+                break
+            abundant_sum_numbers[num1 + num2] = num1 + num2
+    return abundant_sum_numbers
+
+def sum_of_numbers_before_limit(l):
+    return (l-1)*(l)/2
 
 def main():
     abundant_numbers = get_abundant_numbers()
-    print "found_abundant_numbers", len(abundant_numbers)
-    non_abundant_sum_numbers = [] 
-    for num in range(1, limit):
-        if num in abundant_numbers:
-            continue
-        is_sum_of_abundant_numbers = False
-        for diff in range(1, num):
-            if diff in abundant_numbers and num - diff in abundant_numbers:
-                is_sum_of_abundant_numbers = True
-        if not is_sum_of_abundant_numbers:
-            non_abundant_sum_numbers.append(num)
-    print sum(non_abundant_sum_numbers)
+    abundant_sum_numbers = get_abundant_sum_numbers(abundant_numbers)
+    print sum_of_numbers_before_limit(limit) - sum(abundant_sum_numbers.keys())
 
 if __name__ == "__main__":
     main()
