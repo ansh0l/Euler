b06a8385af44602d2d601acc3da90d6225696eae
ansh0l <anshl.goyl@gmail.com>
2014-07-12 18:03:06 +0530

    prob 37 nomenclature changes

--- 037/truncatable_primes.py
+++ 037/truncatable_primes.py
@@ -1,32 +1,32 @@
 primes = [2, 3, 5, 7]
 
-def is_ltr_truncable_prime(n):
-    ltr_truncable_prime = True
+def is_ltr_truncatable_prime(n):
+    ltr_truncatable_prime = True
     string_n = str(n)
     while string_n != '':
         n = int(string_n) #if at end, will throw ValueError on ''
         if n not in primes:
-            ltr_truncable_prime = False
+            ltr_truncatable_prime = False
             break
         string_n = string_n[:-1]
-    return ltr_truncable_prime
+    return ltr_truncatable_prime
 
 
-def is_rtl_truncable_prime(n):
-    rtl_truncable_prime = True
+def is_rtl_truncatable_prime(n):
+    rtl_truncatable_prime = True
     string_n = str(n)
     while string_n != '':
         n = int(string_n) #if at end, will throw ValueError on ''
         if n not in primes:
-            rtl_truncable_prime = False
+            rtl_truncatable_prime = False
             break
         string_n = string_n[1:]
-    return rtl_truncable_prime
+    return rtl_truncatable_prime
 
 def get_next_prime():
     candidate = primes[-1] + 2
-    while candidate not in primes:
-        cab_be_prime = True
+    while True:
+        can_be_prime = True
         for p in primes:
             if p**2 > candidate:
                 break
@@ -42,8 +42,8 @@ def main():
     idx, sigma = 0, 0
     while idx != 11:
         p = get_next_prime()
-        if is_ltr_truncable_prime(p) and is_rtl_truncable_prime(p):
-            print p
+        if is_ltr_truncatable_prime(p) and is_rtl_truncatable_prime(p):
+            print p, idx
             sigma += p
             idx += 1
     print sigma
