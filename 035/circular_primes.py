a3457d7bd9288d42fd3c57bb11a66ef1b5c072de
ansh0l <anshl.goyl@gmail.com>
2014-07-12 16:44:57 +0530

    problem 35 sovled

--- 035/circular_primes.py
+++ 035/circular_primes.py
@@ -1,4 +1,5 @@
 primes = [2]
+circular_primes = []
 LIMIT = 1000000
 
 def get_prime_numbers():
@@ -19,20 +20,30 @@ def rotate(string, n):
     return string[n:] + string[:n]
 
 def is_circular_prime(n):
-    str_n = rotate(str(n), 1)
-    can_be_circular_prime = True
-    while int(str_n) != n:
-        if int(str_n) not in primes:
-            can_be_circular_prime = False
-            break
-        str_n = rotate(str_n, 1)
-    return can_be_circular_prime
+    string_n = rotate(str(n), 1)
+    num = int(string_n)
+    is_cp = True
+    if set('02468').intersection(set(string_n)) != set() and n != 2 :
+        is_cp = False
+    else:
+        while num != n:
+            if num in circular_primes:
+                break
+            elif num not in primes:
+                is_cp = False
+                break
+            string_n = rotate(string_n, 1)
+            num = int(string_n)
+    return is_cp
     
-get_prime_numbers()
+def main():
+    get_prime_numbers()
 
-circular_primes = []
-for p in primes:
-    if is_circular_prime(p):
-        circular_primes.append(p)
+    for p in primes:
+        if is_circular_prime(p):
+            circular_primes.append(p)
+    
+    print circular_primes, len(circular_primes)
 
-print circular_primes
+if __name__ == "__main__":
+    main()
