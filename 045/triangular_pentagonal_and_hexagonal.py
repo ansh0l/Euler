fbf8c23fcf3edae24ac486d8d3d58dec97b47743
ansh0l <anshl.goyl@gmail.com>
2014-07-18 00:26:09 +0530

    problem 45 solved

--- 045/triangular_pentagonal_and_hexagonal.py
+++ 045/triangular_pentagonal_and_hexagonal.py
@@ -1,5 +1,24 @@
+import math
+
+def get_triangle(n):
+    return n*(n + 1)/2
+
+def is_pentagonal(x):
+    n = int((math.sqrt(24*x + 1) + 1)/6)
+    return x > 0 and n*(3*n - 1)/2 == x
+
+def is_hexagonal(x):
+    n = int((math.sqrt(8*x + 1) + 1)/4)
+    return x > 0 and n*(2*n - 1) == x
+
 def main():
-    pass
+    n = 286
+    while True:
+        triangle = get_triangle(n)
+        if is_pentagonal(triangle) and is_hexagonal(triangle):
+            print n, triangle
+            break
+        n += 1
 
 if __name__ == "__main__":
     main()
