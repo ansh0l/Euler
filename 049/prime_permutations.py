58aa2bca9ab90e80cff4b7e3cdfd9ff278777bca
ansh0l <anshl.goyl@gmail.com>
2014-07-27 00:12:33 +0530

    problem 49 trying another approach

--- 049/prime_permutations.py
+++ 049/prime_permutations.py
@@ -1,26 +1,28 @@
-primes = [2]
-
 def get_primes_before_10000():
+    primes = [2]
     candidate = 3
     while candidate < 10000:
         if all(candidate % prime for prime in primes if prime**2 <= candidate):
             primes.append(candidate)
         candidate += 2
+    return primes
 
-def satisfy_permutation(n):
-    str_n = str(n)
-    rotate_by_1 = int(str_n[1:] + str_n[:1])
-    rotate_by_2 = int(str_n[2:] + str_n[:2])
-    return len(str_n) == 4 and len(str(rotate_by_1)) == 4 and len(str(rotate_by_2)) == 4\
-        and rotate_by_1 in primes and rotate_by_2 in primes
-    
+def get_primes_hashing_to_same_keywords(primes):
+    prime_count = {}
+    for prime in primes:
+        key = int(''.join(sorted(list(str(prime)))))
+        if prime_count.has_key(key):
+            prime_count[key].append(prime)
+        else:
+            prime_count[key] = [prime]
+    return prime_count
 
 def main():
-    get_primes_before_10000()
-    for prime in primes:
-        if satisfy_permutation(prime):
-            print prime
-            break
+    primes = get_primes_before_10000()
+    primes_hash = get_primes_hashing_to_same_keywords(primes)
+    for primes in primes_hash.values():
+        if len(primes) > 2:
+            print primes
 
 if __name__ == "__main__":
     main()
