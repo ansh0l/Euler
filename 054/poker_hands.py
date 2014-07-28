c51155f2467d7410270514923ce1fe89a58b1b07
ansh0l <anshl.goyl@gmail.com>
2014-07-29 00:40:32 +0530

    prob 54 more classes

--- 054/poker_hands.py
+++ 054/poker_hands.py
@@ -4,6 +4,9 @@ class InvalidCard(Exception):
 class InvalidHand(Exception):
     pass
 
+class InvalidGame(Exception):
+    pass
+
 class Card(object):
 
     def __init__(self, card='AC'):
@@ -23,9 +26,16 @@ class Hand(object):
             raise InvalidHand
         self.cards = cards
 
+class Game(object):
+
+    def __init__(self, hands = []):
+        if type(hands) != list or len(hands) != 2 or \
+                not all(type(h) == Hand for h in hands):
+            raise InvalidGame
+        self.hands = hands
+
 def main():
     pass
 
 if __name__ == "__main__":
     main()
-
