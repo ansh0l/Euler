71a6e2c4613c680592b8511242554cb6c8d35ca5
ansh0l <anshl.goyl@gmail.com>
2014-07-29 00:05:11 +0530

    prob 53 initial functions

--- 053/combinatoric_selections.py
+++ 053/combinatoric_selections.py
@@ -24,4 +24,16 @@ def find_r_for_n(n):
         r += 1
     return -1
     
+def get_combinatoric_selections(n):
+    r = find_r_for_n(n)
+    return 0 if r == -1 else len(range(r, n + 1 - r))
 
+def main():
+    total_selections = 0
+    for n in range(1, 100 + 1):
+        total_selections += get_combinatoric_selections(n)
+    print total_selections
+
+
+if __name__ == "__main__":
+    main()
