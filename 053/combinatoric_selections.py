7738c24bfad90a52eda856d20f08d4beaf1f2758
ansh0l <anshl.goyl@gmail.com>
2014-07-28 22:15:13 +0530

    prob 53 initial functions

---
+++ 053/combinatoric_selections.py
@@ -0,0 +1,27 @@
+MILLION = 1000000
+
+def get_ncr(n, r):
+    if r > n or r < 0:
+        raise
+    elif 2 * r > n:
+        r = n - r
+    if r == 0:
+        return 1
+    numerator, denominator = n, 1
+    iterator = 2
+    while iterator <= r:
+        numerator *= (n + 1 -iterator)
+        denominator *= iterator
+        iterator += 1
+    return numerator/denominator
+
+def find_r_for_n(n):
+    r, ncr = 0, 1
+    while 2 *r <= n:
+        ncr = get_ncr(n, r)
+        if ncr > MILLION:
+            return r
+        r += 1
+    return -1
+    
+
