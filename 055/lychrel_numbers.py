481ce46bf2f9bdd786c84980bc3c5871d93ce0c5
ansh0l <anshl.goyl@gmail.com>
2014-08-20 20:50:37 +0530

    prob 55 more smallish functions

--- 055/lychrel_numbers.py
+++ 055/lychrel_numbers.py
@@ -1,3 +1,5 @@
+MAX_ITERATIONS = 50
+
 def reverse(num):
     return int(str(num)[::-1])
 
@@ -7,3 +9,20 @@ def is_palindrome(num):
     half_length = length/2
     return string_n[:half_length] ==\
         string_n[length - half_length:][::-1]
+
+def is_lychrel_number(num):
+    new_num = num
+    for i in range(MAX_ITERATIONS):
+        if is_palindrome(new_num):
+            print num, new_num 
+            return True
+        new_num += reverse(new_num)
+    return False
+        
+sigma = 0
+for num in range(0, 10000):
+    if is_lychrel_number(num):
+        sigma += 1
+    else:
+        print num
+print sigma
