c7d84dc07900644169d8d60cfcebdb53ab70ece9
ansh0l <anshl.goyl@gmail.com>
2014-08-21 23:25:09 +0530

    prob 56 solved

---
+++ 056/digital_sums.py
@@ -0,0 +1,11 @@
+biggest_sum = 0
+
+def digital_sum(a, b):
+    return sum([int(d) for d in str(a**b)])
+
+for a in range(1, 101):
+    for b in range(1, 101):
+        sigma = digital_sum(a, b)
+        if sigma > biggest_sum:
+            biggest_sum = sigma
+            print a, b, sigma
