8eefdd4cfc498caa93f904bd8bd881b0487e8244
ansh0l <anshl.goyl@gmail.com>
2014-08-22 00:15:44 +0530

    adding approach for prib 57

--- 057/square_root_convergents.py
+++ 057/square_root_convergents.py
@@ -1,4 +1,4 @@
-from fractions import Fraction as f
+from fractions import Fraction as frac
 
 expression = "(f(1) + (f(1)/f(2)))"
 """
@@ -15,14 +15,11 @@ One way is to replace f(2) with expression, but at the 1000th times, that's too 
 
 The other way si to identify patterns. Numbers we are given:
    3 /   2    |     numerator(n) = denominator(n-1)*2 + numerator(n-1) ; denominator(n) = denominator(n-1) + numerator(n-1)
-   7 /   5    |     7 = 2*2 + 3; 5 = 2 + 3 
-  17 /  12    |   
-  41 /  29    |   
-  99 /  70    |   
- 239 / 169    |   
- 577 / 408    |
-1393 / 985    |
-
-
-
+   7 /   5    |     7 = 2*2+3; 5 = 2+3 
+  17 /  12    |     17 = 2*5+7; 12 = 5+7
+  41 /  29    |     41 = 12*2+17; 29 = 12+17
+  99 /  70    |     99 = 29*2+41; 70 = 29+41
+ 239 / 169    |     239 = 70*2+99; 169 = 70+99
+ 577 / 408    |     577 = 169*2+239; 408 = 169+239
+1393 / 985    |     1393 = 408*2+577; 985 = 577+408
 """
