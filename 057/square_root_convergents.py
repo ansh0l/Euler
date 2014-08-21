1351500d33a83e60475a5d40a7cfedb2e6cb4ff5
ansh0l <anshl.goyl@gmail.com>
2014-08-22 00:10:26 +0530

    adding comments for prib 57

--- 057/square_root_convergents.py
+++ 057/square_root_convergents.py
@@ -4,12 +4,25 @@ expression = "(f(1) + (f(1)/f(2)))"
 """
 One way is to replace f(2) with expression, but at the 1000th times, that's too much nesting
 
-1 + 1/2
-1 + 1/
-      (2 + 1/2)
-f(1) + f(1)/
-            (f(2) + f(1)/
-                        (f(2) + f(1)/f(2)))
+    1 + 1/2
+    1 + 1/
+          (2 + 1/2)
+    f(1) + f(1)/
+                (f(2) + f(1)/
+                            (f(2) + f(1)/f(2)))
+    
+    print eval(expression)
+
+The other way si to identify patterns. Numbers we are given:
+   3 /   2    |     numerator(n) = denominator(n-1)*2 + numerator(n-1) ; denominator(n) = denominator(n-1) + numerator(n-1)
+   7 /   5    |     7 = 2*2 + 3; 5 = 2 + 3 
+  17 /  12    |   
+  41 /  29    |   
+  99 /  70    |   
+ 239 / 169    |   
+ 577 / 408    |
+1393 / 985    |
+
+
 
 """
-print eval(expression)
