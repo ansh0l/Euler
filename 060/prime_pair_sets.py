c4b12d9d2585d83e6ae008b06a7ef4db41f9f713
ansh0l <anshl.goyl@gmail.com>
2014-11-30 23:45:01 +0530

    problem 60 adding solution description

--- 060/prime_pair_sets.py
+++ 060/prime_pair_sets.py
@@ -1,7 +1,34 @@
 # -*- coding: utf-8 -*-
 
 """
-The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes and concatenating them in any order the result will always be prime. For example, taking 7 and 109, both 7109 and 1097 are prime. The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.
+Question:
+    The primes 3, 7, 109, and 673, are quite remarkable.
+    By taking any two primes and concatenating them in any order the result
+    will always be prime. For example, taking 7 and 109, both 7109 and 1097
+    are prime. The sum of these four primes, 792, represents the lowest sum
+    for a set of four primes with this property.
 
-Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
+    Find the lowest sum for a set of five primes for which any two primes
+    concatenate to produce another prime.
 """
+
+"""
+Solution:
+
+This looks like a graph problem so it seems to me.
+
+Essentially, if we connect every two vertices, which have this property
+    (eg 3 -> 7, 3-> 109, 3 -> 673) and so on, then we are looking for a
+    completely connected graph with 5 vertices (i.e., the number of edges
+    must be 5C2 = 10 between the 5 edges)
+
+If we have prime numbers less than X, then total edges = O(n**2)
+The assumption to be made is that most of them won't exist, though this
+ensures the TimeComplexity is O(n**2) always (don't think TC matters much)
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main(
