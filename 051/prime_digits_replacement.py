cc818b4f364096253b984d7490508767d2d29451
ansh0l <anshl.goyl@gmail.com>
2014-12-19 17:53:41 +0530

    adding problem description for problem 51

--- 051/prime_digits_replacement.py
+++ 051/prime_digits_replacement.py
@@ -1,12 +1,31 @@
 """
-Only those numbers, which have digits less than equal to 2 can be replaced effectively
-Example:
-    if number is 562133
-        then only on digits {2, 1} can there be replacements, since minimum 8 replacements are needed.
-        If we try replacing any other digit, they should have already been detected
-Also, in case of multiple digits replacement the number should have repeat digits (only then it can satisfy the rules)
-Finally, the number can either all replacement digits will have to be greater than the key being replaced
+Problem Statement:
+
+By replacing the 1st digit of the 2-digit number *3, it turns out that six of
+the nine possible values: 13, 23, 43, 53, 73, and 83, are all prime.  
+
+By replacing the 3rd and 4th digits of 56**3 with the same digit, this 5-digit
+number is the first example having seven primes among the ten generated numbers,
+yielding the family: 56003, 56113, 56333, 56443, 56663, 56773, and 56993.
+Consequently 56003, being the first member of this family, is the smallest prime
+with this property.  
+
+Find the smallest prime which, by replacing part of the number (not necessarily
+adjacent digits) with the same digit, is part of an eight prime value family.
 """
+
+""" 
+Solution
+
+Only those numbers, which have digits less than equal to 2 can be replaced
+effectively Example: if number is 562133 then only on digits {2, 1} can there be
+replacements, since minimum 8 replacements are needed.  If we try replacing any
+other digit, they should have already been detected Also, in case of multiple
+digits replacement the number should have repeat digits (only then it can
+satisfy the rules) Finally, the number can either all replacement digits will
+have to be greater than the key being replaced 
+
+""" 
 from collections import Counter
 
 with open("primes.million.txt", "r") as f:
