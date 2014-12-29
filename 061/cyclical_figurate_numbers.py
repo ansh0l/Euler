5bd494897bcde9147684c9d1f08e6a425bbf610d
ansh0l <anshl.goyl@gmail.com>
2014-12-29 20:40:10 +0530

    Methods for all geometrical figures

--- 061/cyclical_figurate_numbers.py
+++ 061/cyclical_figurate_numbers.py
@@ -8,43 +8,57 @@ x = (-1*b + sqrt(b**2 - 4*a*c))/(2*a)
 
 def is_triangle(N):
     """
-    A number is Triangle if N = n*(n+1)/2
+    A number is Triangle if N == n*(n+1)/2
     => n**2 + n - 2*N = 0
     => n = (-1 + sqrt(1 + 8*N))/2
     """
     n = int(round((-1 + math.sqrt(1 + 8*N))/2))
-    return n**2 == N
+    return N == n*(n+1)/2
     
 
 def is_square(N):
     """
     A number is Square if N == n**2
     """
-    return int(round(math.sqrt(N)))**2 == N
+    n = int(round(math.sqrt(N)))
+    return N == n**2
 
 def is_pentagonal(N):
     """
-    n*(3*n-1)/2
+    A number is pentagonal if N == n*(3*n-1)/2
+    => 3*n**2 - n -2*N == 0
+    => n = (1 + sqrt(1 + 24*N))/6
     """
-    pass
+    n = int(round((1 + math.sqrt(1 + 24*N))/6))
+    return N == n*(3*n-1)/2
+
 
 def is_hexagonal(N):
     """
-    n*(2*n-1)
+    A number is hexagonal if N == n*(2*n-1)
+    => 2*n**2 - n - N == 0
+    => n = (1 + sqrt(1 + 8*N))/4
     """
-    pass
+    n = int(round((1 + math.sqrt(1 + 8*N))/4))
+    return N == n*(2*n-1)
 
 def is_heptagonal(N):
     """
-    n*(5*n-3)/2
+    A number is heptagonal if N == n*(5*n-3)/2
+    => 5*n**2 - 3*n - 2*N == 0
+    => n = (3 + sqrt(9 + 40*N))/10
     """
-    pass
+    n = int(round((3 + sqrt(9 + 40*N))/10))
+    return N == n*(5*n-3)/2
 
 def is_octagonal(N):
     """
-    n*(3*n-2)
+    A number is octagonal if N == n*(3*n-2)
+    => 3*n**2 - 2*n - N == 0
+    => n = (2 + sqrt(4 + 16*N))/6
     """
-    pass
+    n = int(round((2 + math.sqrt(4 + 16*N))/6))
+    return N == n*(3*n-2)
 
 def main():
     pass
