5eb33162855b8b23b5dc1909d45ce8f7961807d5
ansh0l <anshl.goyl@gmail.com>
2015-01-07 19:52:54 +0530

    documentation changes for prob 63

--- 063/powerful_digits_count.py
+++ 063/powerful_digits_count.py
@@ -5,7 +5,10 @@ For numbers beyond 9, any nth power will have at least n+1 digits.
     Example 10**1 => 2 digits
             10**2 => 3 digits
             10**3 => 4 digits
-So only numbers till 9 need to be counted
+So only numbers till 9 need to be counted.
+
+The other thing to note is that 9*99 has 95 digits.
+So an arbitary range of 100 power has been taken up.
 """
 
 count = 0
