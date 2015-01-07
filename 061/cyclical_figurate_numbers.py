1807c71e8b85dec446aed2d8c6f9fc4ae4168bd6
ansh0l <anshl.goyl@gmail.com>
2015-01-07 17:06:13 +0530

    brute force solution for prob 61

--- 061/cyclical_figurate_numbers.py
+++ 061/cyclical_figurate_numbers.py
@@ -4,7 +4,7 @@ import sys
 from collections import defaultdict
 
 """
-a*x**2 + b*x + c = 0 has the solution 
+a*x**2 + b*x + c = 0 has the solution
 x = (-1*b + sqrt(b**2 - 4*a*c))/(2*a)
 """
 
@@ -78,7 +78,6 @@ is_valid = lambda x: 999 < x < 10000 and 9 < x % 100
 class DD(defaultdict):
     def __init__(self, f=lambda: [], *args, **kwargs):
         return super(DD, self).__init__(f, *args, **kwargs)
-        
 
 def get_numbers():
     tria, squa, pent, hexa, hept, octa = DD(), DD(), DD(), DD(), DD(), DD()
@@ -109,8 +108,36 @@ def get_numbers():
 
     return tria, squa, pent, hexa, hept, octa
 
+def find_cycle(sets, start=[], original=""):
+    """
+    Let numbers be {abcd, cdef, efgh, ghij, ijkl, klab}
+    """
+    abcd = sets[0]
+    for ab in abcd:
+        for cd in abcd[ab]:
+            cdef = sets[1]
+            if cd in cdef:
+                for ef in cdef[cd]:
+                    efgh = sets[2]
+                    if ef in efgh:
+                        for gh in efgh[ef]:
+                            ghij = sets[3]
+                            if gh in ghij:
+                                for ij in ghij[gh]:
+                                    ijkl = sets[4]
+                                    if ij in ijkl:
+                                        for kl in ijkl[ij]:
+                                            klab = sets[5]
+                                            if kl in klab:
+                                                for ab in klab[kl]:
+                                                    if ab in abcd:
+                                                        return ab + cd + ef + ij + kl
+
 def main():
     number_sets = get_numbers()
+    cycle = find_cycle(number_sets)
+    print cycle
+    print sum(map(int, [cycle[0:4],cycle[2:6], cycle[4:8], cycle[6:10], cycle[8:12], cycle[12:] + cycle[:2]]))
 
 if __name__ == "__main__":
     main()
