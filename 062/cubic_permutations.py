b1d5ec81c69174e2464afa15f9634cd83ca6cb7d
ansh0l <anshl.goyl@gmail.com>
2015-01-07 19:12:36 +0530

    adding code for prob 62 solved

--- 062/cubic_permutations.py
+++ 062/cubic_permutations.py
@@ -1,7 +1,16 @@
 import sys
 
+from collections import defaultdict
+
 def main():
-    pass
+    d = defaultdict(lambda: [])
+    for i in range(10000):
+        cube = i**3
+        key = tuple(sorted(str(i**3)))
+        d[key].append(cube)
+        if len(d[key]) > 4:
+            print d[key]
+            break
 
 if __name__ == "__main__":
     main()
