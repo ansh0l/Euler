4bb1e82aa8701c54cea98d8ad9d5e2fbb264ecca
ansh0l <anshl.goyl@gmail.com>
2015-01-21 21:31:40 +0530

    removing redundant methods for prob #61

--- 061/cyclical_figurate_numbers.py
+++ 061/cyclical_figurate_numbers.py
@@ -9,69 +9,11 @@ x = (-1*b + sqrt(b**2 - 4*a*c))/(2*a)
 """
 
 triangle = lambda n: n*(n+1)/2
-
-def is_triangle(N):
-    """
-    A number is Triangle if N == n*(n+1)/2
-    => n**2 + n - 2*N = 0
-    => n = (-1 + sqrt(1 + 8*N))/2
-    """
-    n = int(round((-1 + math.sqrt(1 + 8*N))/2))
-    return N == triangle(n)
-
 square = lambda n: n**2
-
-def is_square(N):
-    """
-    A number is Square if N == n**2
-    """
-    n = int(round(math.sqrt(N)))
-    return N == square(n)
-
 pentagonal = lambda n: n*(3*n-1)/2
-
-def is_pentagonal(N):
-    """
-    A number is pentagonal if N == n*(3*n-1)/2
-    => 3*n**2 - n -2*N == 0
-    => n = (1 + sqrt(1 + 24*N))/6
-    """
-    n = int(round((1 + math.sqrt(1 + 24*N))/6))
-    return N == pentagonal(n)
-
 hexagonal = lambda n: n*(2*n-1)
-
-def is_hexagonal(N):
-    """
-    A number is hexagonal if N == n*(2*n-1)
-    => 2*n**2 - n - N == 0
-    => n = (1 + sqrt(1 + 8*N))/4
-    """
-    n = int(round((1 + math.sqrt(1 + 8*N))/4))
-    return N == hexagonal(n)
-
 heptagonal = lambda n: n*(5*n-3)/2
-
-def is_heptagonal(N):
-    """
-    A number is heptagonal if N == n*(5*n-3)/2
-    => 5*n**2 - 3*n - 2*N == 0
-    => n = (3 + sqrt(9 + 40*N))/10
-    """
-    n = int(round((3 + math.sqrt(9 + 40*N))/10))
-    return N == heptagonal(n)
-
 octagonal = lambda n: n*(3*n-2)
-
-def is_octagonal(N):
-    """
-    A number is octagonal if N == n*(3*n-2)
-    => 3*n**2 - 2*n - N == 0
-    => n = (2 + sqrt(4 + 12*N))/6
-    """
-    n = int(round((2 + math.sqrt(4 + 12*N))/6))
-    return N == octagonal(n)
-
 
 is_valid = lambda x: 999 < x < 10000 and 9 < x % 100
 
@@ -128,6 +70,7 @@ def find_cycle(sets, start=[], original=""):
                           for mn in klmn[kl]:
                             cycle = ab + cd + ef + gh + ij + kl + mn
                             print cycle
+                            print sum(map(int, [cycle[0:4],cycle[2:6], cycle[4:8], cycle[6:10], cycle[8:12], cycle[10:], cycle[12:] + cycle[:2]]))
                             if mn == ab:
                               return ab + cd + ef + gh + ij + kl
 
@@ -137,6 +80,7 @@ def main():
     if cycle:
         print cycle
         print sum(map(int, [cycle[0:4],cycle[2:6], cycle[4:8], cycle[6:10], cycle[8:12], cycle[10:] + cycle[:2]]))
+        print [cycle[0:4],cycle[2:6], cycle[4:8], cycle[6:10], cycle[8:12], cycle[10:] + cycle[:2]]
 
 if __name__ == "__main__":
     main()
